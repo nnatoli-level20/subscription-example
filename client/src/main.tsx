ec20a2d5f360daff053adf6704150e8a2be30905
Nick Natoli <nnatoli@level20.com>
2024-09-18 11:10:20 -0400

    Added apollo client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetBooks {
+        books {
+          title
+          author
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
