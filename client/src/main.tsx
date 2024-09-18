3e86dc4ce246e8ff3bdedb207b13fbb48e53e0d1
Nick Natoli <nnatoli@level20.com>
2024-09-18 15:02:33 -0400

    Added websocket link

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,10 +6,37 @@ import {
   InMemoryCache,
   ApolloProvider,
   gql,
+  split,
+  HttpLink,
 } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4000/subscriptions",
+  })
+);
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
